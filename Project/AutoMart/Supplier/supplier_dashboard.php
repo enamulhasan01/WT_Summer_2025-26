85640705d07bc52969788d840db1837d16a74e95
enamulhasan01 <enamulhasan241@gmail.com>
2026-08-28 03:57:53 +0600

    updated teh supplier dashboard pg for sessigna and logouts

---
+++ Project/AutoMart/Supplier/supplier_dashboard.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+
+if(!isset($_SESSION["email"]) || $_SESSION["role"] !== "Supplier") {
+    header("Location: ../login.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+
+    <title>Owner Dashboard</title>
+</head>
+<body>
+    <h2>Welcome, <?php echo $_SESSION["email"]; ?>!</h2>
+    <h3>Role: <?php echo $_SESSION["role"]; ?></h3>
+    <p>This is the specialized Owner dashboard.</p>
+    
+    
+    <a href="../logout.php"><button>Logout</button></a>
+</body>
+</html>
