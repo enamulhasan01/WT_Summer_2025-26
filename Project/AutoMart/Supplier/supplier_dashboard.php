aff42c0b5299cec173d8c9092523c8b41024debd
enamulhasan01 <enamulhasan241@gmail.com>
2026-08-28 04:11:24 +0600

    uypdate the supplier

--- Project/AutoMart/Supplier/supplier_dashboard.php
+++ Project/AutoMart/Supplier/supplier_dashboard.php
@@ -8,17 +8,19 @@ if(!isset($_SESSION["email"]) || $_SESSION["role"] !== "Supplier") {
 }
 ?>
 
+
 <!DOCTYPE html>
 <html>
 <head>
 
 
-    <title>Owner Dashboard</title>
+
+    <title>Supplier Dashboard</title>
 </head>
 <body>
     <h2>Welcome, <?php echo $_SESSION["email"]; ?>!</h2>
     <h3>Role: <?php echo $_SESSION["role"]; ?></h3>
-    <p>This is the specialized Owner dashboard.</p>
+    <p>This is the specialized Supplier dashboard.</p>
     
     
     <a href="../logout.php"><button>Logout</button></a>
